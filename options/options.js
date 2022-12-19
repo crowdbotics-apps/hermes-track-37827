01ad042331ede965a9f98f0da6a52e094deab882
ShivaPandey02 <shiva.pandey@crowdbotics.com>
2022-12-19 20:03:39 +0530

    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,10 @@
-import { StyleSheet } from "react-native";
-
 export const globalOptions = {
   name: "hermes_track_37827",
   url: "https://hermes_track_37827.botics.co",
   api: "https://hermes_track_37827.botics.co/api/v1"
-}
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     "copy": "Routes available!"
   }
-}
+};
